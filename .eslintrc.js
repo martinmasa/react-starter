093732d2b629cef5398d2a19b466be58822d69f2
Martin M <mtendotnet@users.noreply.github.com>
2018-10-13 10:09:35 +0100

    build: update eslint and prettier config files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,53 +1,47 @@
+const prettierOptions = require('./.prettierrc');
+
+const OFF = 0;
+const ERROR = 2;
+
 module.exports = {
-  "parser": "babel-eslint",
-  "extends": [
-    "airbnb",
-    "prettier"
+  'root': true,
+  'parser': 'babel-eslint',
+  'extends': [
+    'airbnb',
+    'prettier'
   ],
-  "env": {
-    "browser": true,
-    "node": true,
-    "jest": true,
-    "es6": true
+  'env': {
+    'browser': true,
+    'node': true,
+    'jest': true,
+    'es6': true
   },
-  "plugins": [
-    "react",
-    "jsx-a11y",
-    "import",
-    "prettier"
+  'plugins': [
+    'react',
+    'jsx-a11y',
+    'import',
+    'prettier'
   ],
-  "rules": {
-    "prettier/prettier": [
-      "error",
-      {
-        "arrowParens": "always",
-        "bracketSpacing": true,
-        "jsxBracketSameLine": false,
-        "parser": "babylon",
-        "printWidth": 100,
-        "semi": true,
-        "singleQuote": true,
-        "trailingComma": "none"
-      }
-    ],
-    "arrow-body-style": "off",
-    "arrow-parens": "off",
-    "camelcase": "off",
-    "class-methods-use-this": "off",
-    "comma-dangle": "off",
-    "function-paren-newline": "off",
-    "object-curly-newline": "off",
-    "implicit-arrow-linebreak": "off",
-    "import/no-extraneous-dependencies": "off",
-    "import/no-named-as-default": "off",
-    "import/prefer-default-export": "off",
-    "jsx-a11y/anchor-is-valid": "off",
-    "jsx-a11y/label-has-associated-control": "off",
-    "jsx-a11y/label-has-for": "off",
-    "react/prefer-stateless-function": "off",
-    "react/prop-types": "off",
-    "react/require-default-props": "off",
-    "react/jsx-one-expression-per-line": "off",
-    "react/jsx-filename-extension": "off"
+  'rules': {
+    'prettier/prettier': [ERROR, prettierOptions],
+    'arrow-body-style': OFF,
+    'arrow-parens': OFF,
+    'camelcase': OFF,
+    'class-methods-use-this': OFF,
+    'comma-dangle': OFF,
+    'function-paren-newline': OFF,
+    'object-curly-newline': OFF,
+    'implicit-arrow-linebreak': OFF,
+    'import/no-extraneous-dependencies': OFF,
+    'import/no-named-as-default': OFF,
+    'import/prefer-default-export': OFF,
+    'jsx-a11y/anchor-is-valid': OFF,
+    'jsx-a11y/label-has-associated-control': OFF,
+    'jsx-a11y/label-has-for': OFF,
+    'react/prefer-stateless-function': OFF,
+    'react/prop-types': OFF,
+    'react/require-default-props': OFF,
+    'react/jsx-one-expression-per-line': OFF,
+    'react/jsx-filename-extension': OFF
   }
 };
