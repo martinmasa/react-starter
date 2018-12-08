9ece8c74477a173154bbd7f8e253c6d664300c8a
Martin M <mtendotnet@users.noreply.github.com>
2018-12-08 00:07:48 +0000

    feat: add basic @reach/router configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
+import { Router, Link } from '@reach/router';
 
 import GlobalStyle from './global-styles';
 
@@ -7,6 +8,9 @@ const Wrapper = styled.div`
   color: #353535;
   text-align: center;
 `;
+
+const Home = () => <p>This is the Home page.</p>;
+const About = () => <p>This is the About page.</p>;
 
 class App extends Component {
   state = {
@@ -23,6 +27,16 @@ class App extends Component {
         <p data-testid="page-intro">
           Hello {name}! Starter for a react project configured with some basic tools.
         </p>
+
+        <nav>
+          <Link to="/">[Home]</Link>
+          <Link to="/about">[About]</Link>
+        </nav>
+
+        <Router>
+          <Home path="/" />
+          <About path="/about" />
+        </Router>
       </Wrapper>
     );
   }
