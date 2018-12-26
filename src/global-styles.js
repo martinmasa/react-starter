3121144c4308c2ff8699007ccc6382f54cfcf8fe
Martin M <mxa.code@gmail.com>
2018-12-26 13:28:37 +0000

    feat: update css reset

--- src/global-styles.js
+++ src/global-styles.js
@@ -1,20 +1,48 @@
 import { createGlobalStyle } from 'styled-components';
 
-/* eslint no-unused-expressions: 0 */
 const GlobalStyle = createGlobalStyle`
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+
+  html {
+    box-sizing: border-box;
+    font-size: 10px;
+  }
+
   body {
     margin: 0;
     padding: 0;
+    font-size: 1.5rem;
+    line-height: 2;
     font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
       "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
       sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
   }
+  
+  h1, h2, h3, h4, h5, h6, p, ol, ul {
+    margin: 0;
+    padding: 0;
+    font-weight: normal;
+  }
+  
+  ol, ul {
+    list-style: none;
+  }
+
+  a {
+    text-decoration: none;
+    padding-bottom: 2px;
+
+    &:hover {
+      border-bottom: 2px solid;
+    }
+  }
 
   code {
-    font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New",
-      monospace;
+    font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New", monospace;
   }
 `;
 
